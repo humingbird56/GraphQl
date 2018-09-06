4de2e3bfcd2d6364e86b864bd4c8ee64a9d21ea1
andrew <sbstn.andrew56@gmail.com>
2018-09-06 09:54:13 +0700

    make server for tes on browser

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const {graphql, buildSchema } = require('graphql')
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const {graphql, buildSchema } = require('graphql');
+
+const PORT = process.env.Port || 3000;
+const server = express();
 const schema = buildSchema(`
+type Video {
+  id: ID,
+  title: String,
+  duration: Int,
+  watched: Boolean
+}
+
 type Query {
-  foo: String
+  video: Video
+  videos: [Video]
 }
 
 type Schema {
@@ -9,16 +22,39 @@ type Schema {
 }
 `);
 
-const resolvers = {
-  foo: () => 'bar',
+const videoA = {
+  id: 'a',
+  title: 'Create a GraphQl Schema',
+  duration: 120,
+  watched: true,
 };
 
-const query = `
-query myFirstQuery {
-  foo
-}
-  `;
+const videoB = {
+  id: 'b',
+  title: 'Ember.js CLI',
+  duration: 240,
+  watched: false,
+};
 
-graphql(schema, query, resolvers)
-  .then((result) => console.log(result))
-  .catch((error) => console.log(error))
+const videos = [videoA, videoB]
+
+const resolvers = {
+  video: () => ({
+    id: '1',
+    title: 'Foo',
+    duration: 180,
+    watched: true,
+  }),
+
+  videos: () => videos
+};
+
+  server.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true,
+    rootValue: resolvers
+  }))
+
+  server.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`)
+  })
