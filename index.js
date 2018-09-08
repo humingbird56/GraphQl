3c2786acaeaca7a970c714591355b5015e693fa7
andrew <sbstn.andrew56@gmail.com>
2018-09-08 15:26:06 +0700

    error in video 13

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const graphqlHTTP = require('express-graphql');
 const {
   GraphQLSchema,
   GraphQLObjectType,
+  GraphQLList,
+  GraphQLInputObjectType,
+  GraphQLNonNull, //give error for non null
   GraphQLString,
   GraphQLID,
   GraphQLInt,
   GraphQLBoolean
 } = require('graphql');
-const { getVideoById } = require('./src/data')
+const { getVideoById, getVideos, createVideo} = require('./src/data')
+const { globalIdField } = require('graphql-relay');
+const { nodeInterface, nodeField } = require('./src/node')
 
 const PORT = process.env.Port || 3000;
 const server = express();
@@ -17,10 +22,7 @@ const videoType = new GraphQLObjectType({
   name: 'Video',
   description: 'A video in egghead',
   fields: {
-    id: {
-      type: GraphQLID,
-      description: 'The id of the video'
-    },
+    id: globalIdField(),
     title: {
       type: GraphQLString,
       description: 'The title of the video'
@@ -35,15 +37,21 @@ const videoType = new GraphQLObjectType({
     }
   }
 })
+
 const queryType = new GraphQLObjectType({
   name: 'QueryType',
   description: 'The Root query type',
   fields: {
+    node: nodeField,
+    videos: {
+      type: new GraphQLList(videoType),
+      resolve: getVideos,
+    },
     video: {
       type: videoType,
       args: {
         id: {
-          type: GraphQLID,
+          type: new GraphQLNonNull(GraphQLID),
           description: 'The id of the video'
         }
       },
@@ -53,8 +61,49 @@ const queryType = new GraphQLObjectType({
     }
   }
 })
+
+const videoInputType = new GraphQLInputObjectType({
+  name: 'VideoInput',
+  fields: {
+    title: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The title of the video'
+    },
+    duration: {
+      type: new GraphQLNonNull(GraphQLInt),
+      description: 'The duration of the video'
+    },
+    released: {
+      type: new GraphQLNonNull(GraphQLBoolean),
+      description: 'Wheter or not the video is released'
+    }
+  },
+  interfaces: [nodeInterface],
+})
+
+exports.videoType = videoType;
+
+const mutationType = new GraphQLObjectType({
+  name: 'Mutation',
+  description: 'The root Mutation type',
+  fields: {
+    createVideo: {
+      type: videoType,
+      args: {
+        video: {
+          type: new GraphQLNonNull(videoInputType)
+        }
+      },
+      resolve: (_, args) => {
+        return createVideo(args.video);
+      }
+    }
+  }
+})
+
 const schema = new GraphQLSchema({
   query: queryType,
+  mutation: mutationType
 })
 
   server.use('/graphql', graphqlHTTP({
--- src/data/index.js
+++ src/data/index.js
@@ -21,4 +21,27 @@ const getVideoById = (id) => new Promise((resolve) => {
   resolve(video);
 })
 
+const getVideos = () => new Promise((resolve) => resolve(videos))
+const createVideo = ({ title, duration, released }) => {
+  const video = {
+    id: (new Buffer(title, 'utf8')).toString('base64'),
+    title,
+    duration,
+    released
+  };
+
+  videos.push(video);
+  return video;
+}
+const getObjectById = (type, id) => {
+  const types = {
+    video: getVideoById
+  };
+
+  return types[type](id)
+}
+
 exports.getVideoById = getVideoById;
+exports.getVideos = getVideos;
+exports.createVideo = createVideo;
+exports.getObjectById = getObjectById
